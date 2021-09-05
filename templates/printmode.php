343fdf78c731dbb0a0a12887c1ab482240901e2c
Emerson <hi@emersonthis.com>
2021-09-05 10:35:11 -0700

    print mode CSS

--- templates/printmode.php
+++ templates/printmode.php
@@ -1,6 +1,7 @@
 <html>
     <head>
         <link rel="stylesheet" href="<?= plugin_dir_url(dirname(__FILE__)) . 'resource-guide.css' ?>" />
+        <link rel="stylesheet" href="<?= plugin_dir_url(dirname(__FILE__)) . 'resource-guide-printmode.css' ?>" />
     </head>
     <body>
     <?php
